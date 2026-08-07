85e1f422b674838dfecb14c7954c378131b72cb2
inKIVI <51324240+inKIVI@users.noreply.github.com>
2026-08-08 03:02:31 +0700

    Bump SoundCloud player cache version

--- assets/cd-playback-fix.js
+++ assets/cd-playback-fix.js
@@ -5,7 +5,7 @@
   document.head.appendChild(mobileCss);
 
   const player=document.createElement('script');
-  player.src='./assets/player-v3.js?v=20260808b';
+  player.src='./assets/player-v3.js?v=20260808c';
   player.defer=true;
   document.head.appendChild(player);
 
